cf20acbf69afa04d8ab08ccf3faa0f7bc6e23616
MAC37 <mac37@MacBook-Pro-de-MAC37.local>
2022-05-20 13:25:31 -0500

    segundo commit

---
+++ SpotifyClone/Helper/Spotify+Image.swift
@@ -0,0 +1,27 @@
+//
+//  Spotify+Image.swift
+//  SpotifyClone
+//
+//  Created by MAC37 on 18/05/22.
+//
+
+import Foundation
+import UIKit
+
+class HelperImage {
+    
+    static func setImageFromUrl(url: String) -> UIImage? {
+        return convertImageFromURL(url: url)
+    }
+    
+    static func convertImageFromURL(url: String) -> UIImage? {
+        let data = try? Data(contentsOf: HelperString.getURLFromString(url: url)!)
+        
+        if let imageData = data {
+            return UIImage(data: imageData)
+        }
+        
+        return nil
+    }
+    
+}
