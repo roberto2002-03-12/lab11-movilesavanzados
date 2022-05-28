a0acaecb47ee3074e709e7fa06f41bc5a3d1bdb3
LuchoMar123 <betomarina@gmail.com>
2022-05-28 12:52:58 -0500

    primer commit lab11

--- SpotifyClone/View/ViewController.swift
+++ SpotifyClone/View/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTappedLogin(_ sender: Any) {
+        let email = textFieldEmail.text!
+        let password = textFieldPassword.text!
+        
+        login(email: email, password: password)
+    }
+    
+    func login(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) {
+            authResult, error in
+            
+            //si sucede error porque no haya un usuario con esos datos
+            //pasar a registrarlos, sin embargo, no es recomendable porque por
+            //cualquier error puede llamar a la función registro
+            if error != nil {
+                self.register(email: email, password: password)
+            } else {
+                
+            }
+            
+        }
+    }
+    
+    func register(email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) {
+            authResult, error in
+            //todo esta bien si es vacio
+            if error == nil {
+                
+            }
+        }
+    }
+    
 }
 
